17d8a09920470b41999dfa6f02ed9335da70972a
Gabriel Elías Valdelamar Caldera <gvaldelamar2003@outlook.com>
2026-05-05 15:42:00 -0500

    feat: add Docker configuration and Prisma environment setup

--- solicitudes-indirectos/prisma.config.ts
+++ solicitudes-indirectos/prisma.config.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
-    url: env("DATABASE_URL"),   // o directamente "file:./dev.db"
+    url: process.env["DATABASE_URL"],
   },
 });
